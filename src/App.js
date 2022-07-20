794836c9eea2b5b5f81cb407f0c10b400c28b9f7
ratnamala95 <yadav.ratnamala95@gmail.com>
2022-07-20 12:56:59 +0530

    initial commit

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import './App.css';
+import Row from './Row';
+
+function App() {
+  return (
+    <div className="App">
+    </div>
+  );
+}
+
+export default App;
---
+++ src/Row.js
@@ -0,0 +1,18 @@
+import React from 'react';
+
+
+function Row({title}){
+    return(
+        <div>
+            {/* title */}
+            <h2>{title}</h2>
+
+            {/* container -> posters */}
+
+
+
+        </div>
+    );
+}
+
+export default Row;
