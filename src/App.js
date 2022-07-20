6bbfc3edd765cd34328649392fc48786accd99bb
ratnamala95 <yadav.ratnamala95@gmail.com>
2022-07-20 15:16:56 +0530

    commit#2: added images on page

--- src/App.js
+++ src/App.js
@@ -1,9 +1,12 @@
 import './App.css';
 import Row from './Row';
+import requests from './requests';
 
 function App() {
   return (
     <div className="App">
+      <Row title='NETFLIX ORIGINALS' fetchURL={requests.fetchNetflixOriginals} />
+      <Row title='Trending Now' fetchURL={requests.fetchTrending} />
     </div>
   );
 }
--- src/Row.js
+++ src/Row.js
@@ -1,16 +1,50 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import axios from './axios'; //creating an alias we can call it axios instead of instance coz it's a default export
+
+// we need a snippet of code that runs based on a specific condition or variable
 
 
-function Row({title}){
+const baseURL = 'https://image.tmdb.org/t/p/original';
+
+
+function Row({title, fetchURL}){
+
+    const [ movies, setMovies ] = useState([]); // initialised empty array inside useState
+
+    // if we leave the [] empty that means, run once and do not run again
+    // if we give [movies] it means, run once and then run everytime movies changes
+
+    useEffect(()=>{
+        // we cannot directly run an async request inside useEffect
+
+        async function fetchData(){
+            const request = await axios.get(fetchURL);
+            setMovies(request.data.results);
+            return request;
+        }
+        
+        fetchData();    
+    },[fetchURL]);//if there is a variable that is being used inside useEffect
+    // it is muct that it gets included inside [] otherwise it gives warning
+    // since this hook depends on fetchURL it is now a dependency and all the dependencies 
+    // need to be included coz the code needs rerendering when it changes
+    
+    console.table(movies);
     return(
-        <div>
+        <div className='row'>
             {/* title */}
             <h2>{title}</h2>
 
-            {/* container -> posters */}
+            <div className='row__posters'>
+                {/* container -> posters */}
+                {/* we want to map each item to an image tag */}
 
-
-
+                {movies.map(movie => (
+                    <img  
+                    className='row_poster'
+                    src={`${baseURL}${movie.poster_path}`} alt={movie.name} />
+                ))}
+            </div>
         </div>
     );
 }
